b543a9eee42fcbbf55caf9039ab56446a88b559e
janayaC09 <janaya.c1009@gmail.com>
2025-07-31 08:38:21 -0500

    Add files via upload

---
+++ PlanToPlateFinalApp/BackgroundView.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundView.swift
+//  PlanToPlateFinalApp
+//
+//  Created by DPI Student 148 on 7/22/25.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Image("Plan to Plate Background")
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea()
+    }
+}
